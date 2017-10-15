12cef4b5a9696d039a9c554f275371f9a06d731c
OlafurTho <brightsideoflight@gmail.com>
2017-10-15 16:06:06 +0000

    Tested with one number

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -3,12 +3,16 @@ import org.junit.Test;
 
 import junit.framework.Assert;
 
-
 public class CalculatorTest 
 {
 	@Test
 	public void testEmptyString()
 	{
 		Assert.assertEquals(0, Calculator.add(""));
+	}
+	@Test
+	public void testOneNumber()
+	{
+		Assert.assertEquals(1, Calculator.add("1"));
 	}
 }
